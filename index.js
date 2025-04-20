c11ac9ae9e881b3c82542400dde1e1b8fa63291b
sajib689 <jibonraja11@gmail.com>
2025-04-20 22:24:07 +0600

    feat: update CORS configuration and add Vercel deployment settings

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ const app = express()
 const PORT = process.env.PORT || 5000
 const MONGO_URI = process.env.MONGO_URI
 
-app.use(cors())
+app.use(cors({
+    origin: ['https://coffee-house-client-sigma.vercel.app','http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
